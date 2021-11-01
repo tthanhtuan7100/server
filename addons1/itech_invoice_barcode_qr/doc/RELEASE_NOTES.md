4bef2461a04c458d1bff3ea942d3df51c8939a54
tthanhtuan7100 <tthanhtuan7100@gmail.com>
2021-11-01 23:58:35 +0700

    echo "# server" >> README.md git init

---
+++ addons1/itech_invoice_barcode_qr/doc/RELEASE_NOTES.md
@@ -0,0 +1,8 @@
+## Module <itech_invoice_barcode_qr>
+
+#### 27.10.2020
+#### Version 14.0.1.0.0
+##### ADD
+- Code128 barcode to account invoice/bill
+- QR code to account invoice/bill
+
---
+++ addons1/itech_invoice_barcode_qr/models/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#                                                                           #
+#    iTech Co.                                                              #
+#                                                                           #
+#    Copyright (C) 2020-iTech(<https://www.iTech.com.eg>).                  #
+#                                                                           #
+#############################################################################
+
+from . import invoice_barcode_qr
+from . import res_config_settings
